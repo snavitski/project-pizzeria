ac3c9c5862a41ebe4e5db878e820e6a6d55c655a
Siarhei Navitski <sergiej.nowicki@gmail.com>
2022-02-21 12:34:06 +0100

    10.4

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,53 @@ import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
 
 const app = {
+	initPages: function () {
+		const thisApp = this;
+		thisApp.pages = document.querySelector(select.containerOf.pages).children;
+		thisApp.navLinks = document.querySelectorAll(select.nav.links);
+		const idFromHash = window.location.hash.replace("#/", "");
+
+		let pageMatchingHash = thisApp.pages[0].id;
+
+		for (let page of pages) {
+			if (page.id == idFromHash) {
+				pageMatchingHash = page.id;
+				break;
+			}
+		}
+
+		thisApp.activatePage(pageMatchingHash);
+
+		for (let link of thisApp.navLinks) {
+			link.addEventListener("click", function (event) {
+				const clickedElement = this;
+				event.preventDefault();
+				/*  get page id from href attribute */
+				const id = clickedElement.getAttribute("href").replace("#", "");
+
+				/* run thisApp.activayePage with that id */
+				thisApp.activatePage(id);
+				/* change URL hash */
+				window.location.hash = "#/" + id;
+			});
+		}
+	},
+	activatePage: function (pageId) {
+		const thisApp = this;
+		/*
+		 add class "active" to matching pages, remove from non-matching */
+		for (let page of thisApp.pages) {
+			page.classList.toggke(classNames.pages.active, page.id == pageId);
+		}
+		/*
+		 add class "active" to matching links, remove from non-matching */
+		for (let link of thisApp.links) {
+			link.classList.toggle(
+				classNames.nav.active,
+				link.getAttribute("hre") == "#" + pageId
+			);
+		}
+	},
 	initMenu: function () {
 		const thisApp = this;
 		console.log("thisApp.data;", thisApp.data);
@@ -37,6 +84,7 @@ const app = {
 		console.log("settings:", settings);
 		thisApp.initData();
 		thisApp.initCart();
+		thisApp.initPages;
 	},
 	initCart: function () {
 		const thisApp = this;
--- src/js/components/CartProduct.js
+++ src/js/components/CartProduct.js
@@ -1,5 +1,5 @@
-import { select } from "./settings.js";
-import AmountWidget from "./AmountWidget.js";
+import { select } from ".settings.js";
+import AmountWidget from "./AmountWidget";
 
 class CartProduct {
 	constructor(menuProduct, element) {
